13b18da71ef9a1f25cbdb693c8483f90741cdb1f
hilmimuharromi <hilmimuharrom@gmail.com>
2020-09-01 15:24:24 +0700

    ui page peninjauan kembali 3

--- src/pages/perbendaharaan/dashboard/PeninjauanKembali/PerekamanPemberitahuanPK/index.js
+++ src/pages/perbendaharaan/dashboard/PeninjauanKembali/PerekamanPemberitahuanPK/index.js
@@ -1,9 +1,268 @@
-import { React } from "../../../libraries/dependencies";
+import {
+  React,
+  useState,
+  Form,
+  Upload,
+  Button,
+  Input,
+  UploadOutlined,
+  DatePicker,
+  Select,
+  MinusCircleOutlined,
+  PlusOutlined,
+} from "../../../libraries/dependencies";
 
+const { TextArea } = Input;
+const { Option } = Select;
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 0 },
+  labelAlign: "left",
+};
+
+const tailLayoutLarge = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 0 },
+};
+
+const formItemLayout = {
+  labelCol: { span: 8 },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 20 },
+  },
+  labelAlign: "left",
+};
+const formItemLayoutWithOutLabel = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 0, offset: 8 },
+  labelAlign: "left",
+};
 export default function PerekamanPemberitahuanPK(props) {
+  const [form] = Form.useForm();
+  const [fileList, setFileList] = useState([]);
+
+  const formData = new FormData();
+  fileList.forEach((file) => {
+    formData.append("files[]", file);
+  });
+
+  const submitData = (values) => {
+    console.log(console.log(values));
+    console.log(formData.getAll("files[]"));
+  };
+
+  const uploadHandler = {
+    onRemove: (file) => {
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      return setFileList(newFileList);
+    },
+    beforeUpload: (file) => {
+      const newData = [...fileList, file];
+      setFileList(newData);
+      return false;
+    },
+    fileList,
+  };
+
+  const resetForm = () => {
+    setFileList([]);
+    form.resetFields();
+  };
+
   return (
-    <h2 hidden={props.hidden}>
-      Perekaman Pemberitahuan Permohonan Peninjauan Kembali
-    </h2>
+    <div hidden={props.hidden}>
+      <h2>Perekaman Pemberitahuan Permohonan Peninjauan Kembali</h2>
+      <Form form={form} onFinish={submitData} {...layout}>
+        <Form.Item
+          {...tailLayoutLarge}
+          label="No/Tanggal Objek Banding/Gugatan"
+          style={{ margin: "0" }}
+        >
+          <Input.Group compact>
+            <Form.Item name="noPutusanPP">
+              <Input />
+            </Form.Item>
+            <Form.Item name="tanggalPutusanPP" style={{ margin: "0 5px" }}>
+              <Input />
+            </Form.Item>
+            <Form.Item>
+              <DatePicker />
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
+
+        <Form.Item
+          {...tailLayoutLarge}
+          label="Pemohon Banding/Gugatan"
+          style={{ margin: "0" }}
+        >
+          <Input.Group compact>
+            <Form.Item name="noPemohon">
+              <Input />
+            </Form.Item>
+            <Form.Item name="tanggalPemohon" style={{ margin: "0 5px" }}>
+              <Input />
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
+
+        <Form.Item
+          {...tailLayoutLarge}
+          label="Pokok Sengketa"
+          style={{ margin: "0" }}
+        >
+          <Form.Item name="pokokSengketa">
+            <Input />
+          </Form.Item>
+        </Form.Item>
+
+        <Form.Item
+          {...tailLayoutLarge}
+          label="No Putusan PP"
+          style={{ margin: "0" }}
+        >
+          <Input.Group compact>
+            <Form.Item name="noUtusanPP">
+              <Input />
+            </Form.Item>
+            <Form.Item name="tanggalPutusanPP" style={{ marginLeft: "5px" }}>
+              <DatePicker />
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
+
+        <Form.Item
+          {...tailLayoutLarge}
+          name="hasilAnalisa"
+          label="Hasil Analisa dan Kesimpulan Penolakan"
+        >
+          <TextArea />
+        </Form.Item>
+
+        <Form.Item
+          {...tailLayoutLarge}
+          label="Amar Putusan PP"
+          name="amarPutusanPP"
+        >
+          <Select>
+            <Option>Menolak</Option>
+            <Option>Mengabulkan Seluruhnya</Option>
+            <Option>Mengabulkan Sebagian</Option>
+            <Option>Tidak Dapat Diterima</Option>
+            <Option>Membatalkan</Option>
+            <Option>Mencabut</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item
+          {...tailLayoutLarge}
+          label="No/Tanggal Surat Pemberitahuan PK"
+        >
+          <Input.Group compact>
+            <Form.Item>
+              <Input />
+            </Form.Item>
+            <Form.Item style={{ margin: "0 5px" }}>
+              <Input />
+            </Form.Item>
+            <Form.Item>
+              <p style={{ color: "red" }}>
+                <span>*Tanggal Jatuh tempo 30hr sejak tanggal</span>
+                <br />
+                <span> diterima surat pemberitahuan</span>
+              </p>
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
+
+        <Form.Item
+          {...tailLayoutLarge}
+          label="Tanggal Terima Surat Pemberitahuan PK"
+          name="tanggalTerimaSuratPemberitahuanPK"
+        >
+          <DatePicker />
+        </Form.Item>
+
+        <Form.List
+          name="nipPemeriksaMPK"
+          label="Nip Pemeriksa MPK"
+          {...formItemLayoutWithOutLabel}
+        >
+          {(fields, { add, remove }) => {
+            return (
+              <div>
+                {fields.map((field, index) => (
+                  <Form.Item
+                    {...(index === 0
+                      ? formItemLayout
+                      : formItemLayoutWithOutLabel)}
+                    label={index === 0 ? "Nip Pemeriksa MPK" : ""}
+                    required={false}
+                    key={field.key}
+                  >
+                    <Input.Group compact>
+                      <Form.Item
+                        {...field}
+                        // validateTrigger={["onChange", "onBlur"]}
+                        name={[field.name, `NIP${index}`]}
+                      >
+                        <Input placeholder="NIP" />
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        // validateTrigger={["onChange", "onBlur"]}
+                        name={[field.name, "namaPegawai"]}
+                      >
+                        <Input
+                          placeholder="Nama Pegawai"
+                          style={{ marginLeft: "5px" }}
+                        />
+                      </Form.Item>
+                      {fields.length > 1 ? (
+                        <MinusCircleOutlined
+                          className="dynamic-delete-button"
+                          style={{ margin: "0 8px" }}
+                          onClick={() => {
+                            remove(field.name);
+                          }}
+                        />
+                      ) : null}
+                    </Input.Group>
+                  </Form.Item>
+                ))}
+                <Form.Item {...formItemLayoutWithOutLabel}>
+                  <Button
+                    type="dashed"
+                    onClick={() => {
+                      add();
+                    }}
+                    style={{ width: "60%" }}
+                  >
+                    <PlusOutlined /> Tambah Pegawai
+                  </Button>
+                </Form.Item>
+              </div>
+            );
+          }}
+        </Form.List>
+
+        <Form.Item name="uploadBerkas" label="Upload Berkas">
+          <Upload {...uploadHandler}>
+            <Button>
+              <UploadOutlined />
+              Upload Berkas
+            </Button>
+          </Upload>
+        </Form.Item>
+
+        <Form.Item>
+          <Button htmlType="submit">Simpan</Button>
+          <Button onClick={() => resetForm()}>Cancel</Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
--- src/pages/perbendaharaan/dashboard/PeninjauanKembali/PerekamanPenerimaanNDUsulanPK/index.js
+++ src/pages/perbendaharaan/dashboard/PeninjauanKembali/PerekamanPenerimaanNDUsulanPK/index.js
@@ -1,13 +1,189 @@
-import { React, Form, Select, Layout } from "../../../libraries/dependencies";
+import {
+  React,
+  useState,
+  Form,
+  Upload,
+  Button,
+  Select,
+  Input,
+  MinusCircleOutlined,
+  PlusOutlined,
+  UploadOutlined,
+} from "../../../libraries/dependencies";
 
 const { Option } = Select;
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 4 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 20 },
+  },
+  labelAlign: "left",
+};
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: { span: 24, offset: 0 },
+    sm: { span: 20, offset: 4 },
+  },
+  labelAlign: "left",
+};
 
 export default function PerekamanPenerimaanNDUsulanPK(props) {
+  const [form] = Form.useForm();
+  const [fileList, setFileList] = useState([]);
+
+  const formData = new FormData();
+  fileList.forEach((file) => {
+    formData.append("files[]", file);
+  });
+
+  const submitData = (values) => {
+    console.log(console.log(values));
+    console.log(formData.getAll("files[]"));
+  };
+
+  const uploadHandler = {
+    onRemove: (file) => {
+      const index = fileList.indexOf(file);
+      const newFileList = fileList.slice();
+      newFileList.splice(index, 1);
+      return setFileList(newFileList);
+    },
+    beforeUpload: (file) => {
+      const newData = [...fileList, file];
+      setFileList(newData);
+      return false;
+    },
+    fileList,
+  };
+
   return (
-    <Layout>
-      <h2 hidden={props.hidden}>
-        Perekaman Pemberitahuan Permohonan Peninjauan Kembali
-      </h2>
-    </Layout>
+    <div hidden={props.hidden}>
+      <h2>Perekaman Pemberitahuan Permohonan Peninjauan Kembali</h2>
+      <Form
+        onFinish={submitData}
+        initialValues={{
+          noPutusanPP: "PUT.73190/PP/M.IV/15/2016",
+          tanggalPutusanPP: "22/01/2020",
+          amarPutusanPP: "Menolak",
+          noNDUsulanPK: "S-00001/PP/2020",
+          nipPemeriksaMPK: [{ NIP: "", namaPegawai: "" }],
+        }}
+      >
+        <Form.Item label="No Putusan PP" {...formItemLayout}>
+          <Input.Group compact>
+            <Form.Item name="noPutusanPP">
+              <Input />
+            </Form.Item>
+            <Form.Item name="tanggalPutusanPP">
+              <Input style={{ marginLeft: "5px" }} />
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
+
+        <Form.Item
+          label="Amar Putusan PP"
+          name="amarPutusanPP"
+          {...formItemLayout}
+        >
+          <Select>
+            <Option value="Menolak">Menolak</Option>
+            <Option>Mengaabulkan Seluruhnya</Option>
+            <Option>Mengabulkan Sebagian</Option>
+            <Option>Tidak Dapat Diterima</Option>
+            <Option>Membatalkan</Option>
+            <Option>Mencabut</Option>
+          </Select>
+        </Form.Item>
+
+        <Form.Item label="No/Tanggal ND Usulan PK" {...formItemLayout}>
+          <Input.Group compact>
+            <Form.Item name="noNDUsulanPK">
+              <Input />
+            </Form.Item>
+            <Form.Item name="tanggalNDUsulanPK">
+              <Input style={{ marginLeft: "5px" }} />
+            </Form.Item>
+          </Input.Group>
+        </Form.Item>
+
+        <Form.List name="nipPemeriksaMPK" label="Nip Pemeriksa MPK">
+          {(fields, { add, remove }) => {
+            return (
+              <div>
+                {fields.map((field, index) => (
+                  <Form.Item
+                    {...(index === 0
+                      ? formItemLayout
+                      : formItemLayoutWithOutLabel)}
+                    label={index === 0 ? "Nip Pemeriksa MPK" : ""}
+                    required={false}
+                    key={field.key}
+                  >
+                    <Input.Group compact>
+                      <Form.Item
+                        {...field}
+                        // validateTrigger={["onChange", "onBlur"]}
+                        name={[field.name, `NIP${index}`]}
+                      >
+                        <Input placeholder="NIP" />
+                      </Form.Item>
+                      <Form.Item
+                        {...field}
+                        // validateTrigger={["onChange", "onBlur"]}
+                        name={[field.name, "namaPegawai"]}
+                      >
+                        <Input
+                          placeholder="Nama Pegawai"
+                          style={{ marginLeft: "5px" }}
+                        />
+                      </Form.Item>
+                      {fields.length > 1 ? (
+                        <MinusCircleOutlined
+                          className="dynamic-delete-button"
+                          style={{ margin: "0 8px" }}
+                          onClick={() => {
+                            remove(field.name);
+                          }}
+                        />
+                      ) : null}
+                    </Input.Group>
+                  </Form.Item>
+                ))}
+                <Form.Item {...formItemLayoutWithOutLabel}>
+                  <Button
+                    type="dashed"
+                    onClick={() => {
+                      add();
+                    }}
+                    style={{ width: "60%" }}
+                  >
+                    <PlusOutlined /> Tambah Pegawai
+                  </Button>
+                </Form.Item>
+              </div>
+            );
+          }}
+        </Form.List>
+
+        <Form.Item label="Upload" name="upload" {...formItemLayout}>
+          <Upload {...uploadHandler}>
+            <Button icon={<UploadOutlined />}>Upload Berkas</Button>
+          </Upload>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" onClick={() => form.resetFields()}>
+            Cancel
+          </Button>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 }
