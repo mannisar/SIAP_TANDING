692b8ae3d9802fb10cd783a0828b2bb7d8be3ea7
mannisar <salmanyas3023@gmail.com>
2020-09-03 09:41:39 +0700

    add dummy data

--- src/pages/perbendaharaan/assets/dummy/data.js
+++ src/pages/perbendaharaan/assets/dummy/data.js
@@ -1,3 +1,157 @@
-export const data = [
-
-]
+export const options_keberatan = [
+    {
+        kodeAkun: "411122",
+        uraian: "PPh Pasal 22 Ekspor",
+        key: 1
+    },
+    {
+        kodeAkun: 411123,
+        uraian: "PPH Impor",
+        key: 2
+    },
+    {
+        kodeAkun: 411211,
+        uraian: "PPN HT / DN",
+        key: 3
+    },
+    {
+        kodeAkun: 411212,
+        uraian: "PPN Impor",
+        key: 4
+    },
+    {
+        kodeAkun: 411511,
+        uraian: "Cukai HT",
+        key: 5
+    },
+    {
+        kodeAkun: 411512,
+        uraian: "Cukai EA",
+        key: 6
+    },
+    {
+        kodeAkun: 411513,
+        uraian: "Cukai MMEA",
+        key: 7
+    },
+    {
+        kodeAkun: 411514,
+        uraian: "Denda Administrasi Cukai",
+        key: 8
+    },
+    {
+        kodeAkun: 411519,
+        uraian: "Pendapatan Cukai Lainnya",
+        key: 9
+    },
+    {
+        kodeAkun: 411622,
+        uraian: "Bunga Penagihan PPN",
+        key: 10
+    },
+    {
+        kodeAkun: 412111,
+        uraian: "Bea Masuk",
+        key: 11
+    },
+    {
+        kodeAkun: 412112,
+        uraian: "Bea Masuk ditanggung Pemerintah atas Hibah (SPM) Nihil",
+        key: 12
+    },
+    {
+        kodeAkun: 412113,
+        uraian: "Denda Administrasi Pabean",
+        key: 13
+    },
+    {
+        kodeAkun: 412114,
+        uraian: "Bea Masuk KITE",
+        key: 14
+    },
+    {
+        kodeAkun: 412115,
+        uraian: "Denda administrasi atas pengangkutan barang tertentu",
+        key: 15
+    },
+    {
+        kodeAkun: 412116,
+        uraian: "Pendapatan Bea Masuk Ditanggung Pemerintah (BM-DTP)",
+        key: 16
+    },
+    {
+        kodeAkun: 412119,
+        uraian: "Pendapatan Pabean Lainnya",
+        key: 17
+    },
+    {
+        kodeAkun: 412121,
+        uraian: "Bea Masuk Anti Dumping (BMAD)",
+        key: 18
+    },
+    {
+        kodeAkun: 412122,
+        uraian: "Bea Masuk Imbalan (BMI)",
+        key: 19
+    },
+    {
+        kodeAkun: 412123,
+        uraian: "Bea Masuk Tindakan Pengamanan (BMTP)",
+        key: 20
+    },
+    {
+        kodeAkun: 412211,
+        uraian: "Bea Keluar",
+        key: 21
+    },
+    {
+        kodeAkun: 412212,
+        uraian: "Denda Administrasi Bea Keluar",
+        key: 22
+    },
+    {
+        kodeAkun: 412213,
+        uraian: "Bunga Bea Keluar",
+        key: 23
+    },
+    {
+        kodeAkun: 423216,
+        uraian: "-",
+        key: 24
+    },
+    {
+        kodeAkun: 424138,
+        uraian: "Dana Sawit",
+        key: 25
+    },
+    {
+        kodeAkun: 425151,
+        uraian: "Pendapatan dari Penggunaan Sarana dan Prasarana Sesuai dengan Tusi",
+        key: 26
+    },
+    {
+        kodeAkun: 425289,
+        uraian: "Pendapatan Pengujian, Sertifikasi, Kalibrasi dan Standarisasi Lainnya",
+        key: 27
+    },
+    {
+        kodeAkun: 425699,
+        uraian: "Pendapatan Jasa Lainnya",
+        key: 28
+    },
+    {
+        kodeAkun: 425781,
+        uraian: "Pendapatan Biaya Penagihan Pajak Negara dengan Surat Paksa",
+        key: 29
+    },
+    {
+        kodeAkun: 425839,
+        uraian: "Pendapatan Denda lainnya",
+        key: 30
+    },
+    {
+        kodeAkun: 817711,
+        uraian: "PajakRokok",
+        key: 31
+    }
+];
