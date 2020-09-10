37a89423d5b0e5a2ba320a7af9723ebefd627d92
mannisar <salmanyas3023@gmail.com>
2020-09-10 10:03:35 +0700

    add dummy data

---
+++ src/pages/perbendaharaan/assets/dummy/pungutanKeberatan.js
@@ -0,0 +1,74 @@
+export const pungutanKeberatan = [
+    {
+        kodeAkun: "412111",
+        uraian: "BM",
+    },
+    {
+        kodeAkun: "412211",
+        uraian: "BK",
+    },
+    {
+        kodeAkun: "412123",
+        uraian: "BMTP",
+    },
+    {
+        kodeAkun: "000000",
+        uraian: "BMTPS",
+    },
+    {
+        kodeAkun: "412121",
+        uraian: "BMAD",
+    },
+    {
+        kodeAkun: "000000",
+        uraian: "CTEM",
+    },
+    {
+        kodeAkun: "000000",
+        uraian: "PPN",
+    },
+    {
+        kodeAkun: "411512",
+        uraian: "CEA",
+    },
+    {
+        kodeAkun: "411122",
+        uraian: "PPh Pasal 22",
+    },
+    {
+        kodeAkun: "411513",
+        uraian: "Cukai MMEA",
+    },
+    {
+        kodeAkun: "000000",
+        uraian: "PPnBM",
+    },
+    {
+        kodeAkun: "412122",
+        uraian: "BMI",
+    },
+    {
+        kodeAkun: "411519",
+        uraian: "CK Lain",
+    },
+    {
+        kodeAkun: "000000",
+        uraian: "DENDA PAB",
+    },
+    {
+        kodeAkun: "411514",
+        uraian: "DENDA CK",
+    },
+    {
+        kodeAkun: "000000",
+        uraian: "BUNGA AWAL",
+    },
+    {
+        kodeAkun: "411622",
+        uraian: "BUNGA PPN",
+    },
+    {
+        kodeAkun: "000000",
+        uraian: "PAB LAINNYA (BUNGA)",
+    }
+];
